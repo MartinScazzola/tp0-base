f5344994b1ddb599719ff610f0105c8b4d1136d1
Martin <mscazzola@fi.uba.ar>
2024-09-01 23:08:37 -0300

    prueba fix tests

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -6,7 +6,7 @@ MESSAGE="Hello Server"
 
 RESPONSE=$(docker run --rm --network ${NETWORK_NAME} busybox sh -c "echo '${MESSAGE}' | nc ${SERVER_HOST} ${SERVER_PORT}")
 
-if [ "$RESPONSE" == "$MESSAGE" ]; then
+if [ "$RESPONSE" = "$MESSAGE" ]; then
     echo "action: test_echo_server | result: success"
 else
     echo "action: test_echo_server | result: fail"
